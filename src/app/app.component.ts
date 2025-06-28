4fe5ebb6483057d891b709b7fd2b96b31cf95dc9
Tomasz Jąder <tomasz.jader@gmail.com>
2025-06-28 23:48:07 +0200

    cleaned app html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TablesComponent } from "./componet/tables/tables.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, TablesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
