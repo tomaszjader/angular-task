e813a1408d9607c5a05b6a89fe86ee8d80fcdad4
Tomasz Jąder <tomasz.jader@gmail.com>
2025-07-12 23:45:07 +0200

    fixed

--- src/app/models/dialog-data.model.ts
+++ src/app/models/dialog-data.model.ts
@@ -1,5 +1,11 @@
-import { PeriodicElement } from './periodic-element.model';
+//import { PeriodicElement } from './periodic-element.model';
 
+export interface PeriodicElement { 
+  position: number;
+  name: string;
+  weight: number;
+  symbol: string;
+}
 export interface DialogData {
   element: PeriodicElement;
   field: keyof PeriodicElement;
